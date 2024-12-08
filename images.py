076ec0c8924396dea2e901b93f86a7067d3215ac
Julianizceja <111949490+Julianizceja@users.noreply.github.com>
2024-12-07 23:05:22 -0800

    Category Progress

--- images.py
+++ images.py
@@ -3,10 +3,13 @@ from PIL import Image, ImageSequence
 import io
 
 # Paths to your custom .gif images
-IMAGE_A_PATH = 'https://i.pinimg.com/originals/67/93/45/679345808c0a740e7aec8ff270192d23.gif'  # Hot weather GIF
-IMAGE_B_PATH = 'https://media.tenor.com/EzU33OqujNsAAAAi/glaceon-eevee.gif'  # Cool weather GIF
-IMAGE_C_PATH = 'https://www.shinyhunters.com/images/regular/134.gif'  # Rainy weather GIF
+Pokemon_A_PATH = 'https://i.pinimg.com/originals/67/93/45/679345808c0a740e7aec8ff270192d23.gif'  # Hot weather GIF
+Pokemon_B_PATH = 'https://media.tenor.com/EzU33OqujNsAAAAi/glaceon-eevee.gif'  # Cool weather GIF
+Pokemon_C_PATH = 'https://www.shinyhunters.com/images/regular/134.gif'  # Rainy weather GIF
 IMAGE_SNOWY_PATH = ''  # Snowy weather image (optional)
+Import_A_PATH = ''
+Import_B_PATH = ''
+Import_C_PATH = ''
 
 
 def download_image(image_url):
@@ -24,21 +27,20 @@ def get_image_based_on_weather(weather_data, theme="Default"):
 
     # Theme-based image selection
     if theme == "Rainy":
-        image_path = IMAGE_C_PATH
+        image_path = Pokemon_C_PATH
     elif theme == "Sunny":
-        image_path = IMAGE_A_PATH
+        image_path = Pokemon_A_PATH
     elif theme == "Snowy":
         image_path = IMAGE_SNOWY_PATH
     else:
         # Default behavior
         if 'rain' in weather_description:
-            image_path = IMAGE_C_PATH
+            image_path = Pokemon_C_PATH
         elif temperature >= 80:
-            image_path = IMAGE_A_PATH
+            image_path = Pokemon_A_PATH
         elif temperature <= 70:
-            image_path = IMAGE_B_PATH
+            image_path = Pokemon_B_PATH
         else:
-            image_path = IMAGE_A_PATH
+            image_path = Pokemon_A_PATH
 
     return download_image(image_path)
-
